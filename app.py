909f844021742be6003ad1d2c5476ee1ddefff22
luckhuyen78-stack <luckhuyen78@gmail.com>
2025-12-12 15:53:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -190,8 +190,8 @@ st.markdown("""
 # --- 4. GIAO DIỆN CHÍNH ---
 st.markdown("""
 <div class="main-header">
-    <h1>📘 TRỢ LÝ SOẠN GIÁO ÁN TỰ ĐỘNG (NLS)</h1>
-    <p>Tác giả: Sùng Văn Quý - Trường PTDTBT Tiểu học Đường Thượng - ĐT: 097 4109 364</p>
+    <h1>📘 TRỢ LÝ SOẠN GIÁO ÁN TỰ ĐỘNG Lớp 2 (khuyên)
+    <p>Tác giả: Lục Thị Khuyên - Trường PTDTBT Tiểu học Mậu Long - ĐT: 0988470453</p>
 </div>
 """, unsafe_allow_html=True)
 
@@ -347,4 +347,4 @@ if st.button("🚀 SOẠN GIÁO ÁN NGAY"):
 
 # --- CHÂN TRANG ---
 st.markdown("---")
-st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Sùng Văn Quý - Trường PTDTBT Tiểu học Đường Thượng - ĐT: 097 4109 364</div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Lục thị Khuyên - Trường PTDTBT Tiểu học Mậu Long - ĐT: 0988470453</div>", unsafe_allow_html=True)
